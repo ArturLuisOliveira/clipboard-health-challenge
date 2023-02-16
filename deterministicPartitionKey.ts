d8790ee9fbbdd4538333b9f7c932b5e02fc9551e
ArturLuisOliveira <arturluisoliveira@gmail.com>
2023-02-16 16:58:02 -0300

    wip

--- deterministicPartitionKey.ts
+++ deterministicPartitionKey.ts
@@ -1,28 +1,21 @@
 import crypto from "crypto";
 
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+
 export const deterministicPartitionKey = (event: any) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  const candidate = findCandidate(event);
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH) return hash(candidate);
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
   return candidate;
+};
+
+const hash = (payload: any) =>
+  crypto.createHash("sha3-512").update(payload).digest("hex");
+
+const findCandidate = (event: any) => {
+  if (event.partitionKey) return event.partitionKey;
+  if (event) return hash(JSON.stringify(event));
+  return TRIVIAL_PARTITION_KEY;
 };
