296e3db54a666676060265b1e6dce259dbb1e2ad
ArturLuisOliveira <arturluisoliveira@gmail.com>
2023-02-16 17:02:26 -0300

    improve return statements and add description

---
+++ README.md
@@ -0,0 +1,1 @@
+The purpose main purpose of this refactoring is to reduce the nested if complexity by adding returning statements instead. This made the readability better and the code more maintainable.
--- deterministicPartitionKey.ts
+++ deterministicPartitionKey.ts
@@ -15,7 +15,11 @@ const hash = (payload: any) =>
   crypto.createHash("sha3-512").update(payload).digest("hex");
 
 const findCandidate = (event: any) => {
-  if (event.partitionKey) return event.partitionKey;
+  if (event?.partitionKey) {
+    if (typeof event.partitionKey === "string") return event.partitionKey;
+    return JSON.stringify(event.partitionKey);
+  }
   if (event) return hash(JSON.stringify(event));
+
   return TRIVIAL_PARTITION_KEY;
 };
